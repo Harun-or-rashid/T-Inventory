2ad9f169470f50f2a24ddaa7ab6c34ff9513d570
Harun-or-rashid <harun35-2093@diu.com.bd>
2022-02-08 04:20:42 +0600

    removed unusual codes and files

--- resources/views/backend/product_stock/_get_products.blade.php
+++
@@ -1,9 +0,0 @@
-
-@if(count($products) > 0)
-    <option value="">--Select Product--</option>
-    @foreach($products as $product)
-        <option value="{!! $product->id !!}">{!! $product->title !!}</option>
-    @endforeach
-@else
-    <option value="">No-Product-Available</option>
-@endif
